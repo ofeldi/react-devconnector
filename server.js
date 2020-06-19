3ef905146c74ff316b97dd94d8c295b2efc64a50
Oren Feldman <Oren.Feldman@telit.com>
2020-06-19 22:49:36 +0300

    remove git ignore files

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express =require ('express');
 const mongoose = require ('mongoose');
 
+users = require ('./routes/api/users.js');
+profiles = require ('./routes/api/profiles.js');
+posts = require ('./routes/api/posts.js');
+
+
 const app = express();
 
 //db config
@@ -15,6 +20,11 @@ mongoose
 app.get('/',(req,res) => {
     res.send("hello");
 });
+
+// Routes
+app.use('/api/users', users);
+app.use('/api/profiles', profiles);
+app.use('/api/posts', posts);
 
 const port = process.env.port || 5000;
 
